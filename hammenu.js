067d0a1e3bb6719aa75b29cd204dff3eba607bd3
DennyLei <leidenny1@gmail.com>
2025-04-12 19:52:51 -0700

    Delete hammenu.js

--- hammenu.js
+++
@@ -1,8 +0,0 @@
-const hamMenu = document.querySelector('.ham-menu');
-
-const offScreenMenu = document.querySelector('.off-screen-menu');
-
-hamMenu.addEventLister('click', () {
-  hamMenu.classlist.toggle('active');
-  offScreenMenu.classList.toggle('active');
-})
